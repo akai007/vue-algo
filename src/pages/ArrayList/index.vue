2dc61ee3974d6ce6ff9f766f509ced4ca7033e58
002002 <liaokai@fcbox.com>
2021-05-14 16:17:24 +0800

    add action btn

--- src/pages/ArrayList/index.vue
+++ src/pages/ArrayList/index.vue
@@ -1,5 +1,11 @@
 <template>
   <ArrayList ref="arrayList" v-model="array"></ArrayList>
+  <button class="array-list__action" @click="reset()">reset</button>
+  <button class="array-list__action" @click="arrayList?.insertionSort()">insertion<br />sort</button>
+  <button class="array-list__action" @click="arrayList?.selectionSort()">selection<br />sort</button>
+  <button class="array-list__action" @click="arrayList?.bubbleSort()">bubble<br />sort</button>
+  <button class="array-list__action" @click="arrayList?.quickSort()">quick<br />sort</button>
+  <button class="array-list__action" @click="arrayList?.mergeSort()">merge<br />sort</button>
 </template>
 
 <script setup lang="ts">
@@ -8,7 +14,13 @@ import { onMounted, reactive, ref, useContext } from '@vue/runtime-core';
 import { IArrayList } from '@/components/ArrayList/index.d';
 const arrayList = ref<IArrayList | undefined>(undefined);
 
-let array = reactive([11, 13, 7, 12, 16, 9, 24, 5, 10, 3]);
+const initValue = [11, 13, 7, 12, 16, 9, 24, 5, 10, 3];
+
+let array = reactive([...initValue]);
+
+const reset = function () {
+  array = [...initValue];
+};
 
 onMounted(async () => {
   const arrayListRef = arrayList.value;
@@ -18,7 +30,7 @@ onMounted(async () => {
   // arrayListRef.pop();
 
   // arrayListRef.quickSort();
-  await arrayListRef?.quickSort();
+  // await arrayListRef?.quickSort();
   console.log('done');
 
   // await arrayListRef.quickSort();
@@ -27,4 +39,23 @@ onMounted(async () => {
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.array-list__action {
+  position: relative;
+  display: inline-block;
+  width: 4rem;
+  height: 4rem;
+  text-align: center;
+  border-radius: 100%;
+
+  font-size: 0.8rem;
+
+  margin-left: 10px;
+  &:hover {
+    border-radius: 100%;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
