4293dcee345f0742256a33dac00939cd278354e2
akai <liaokaigogo@foxmail.com>
2021-05-23 10:07:56 +0800

    lint code

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      { find: '@', replacement: '/src' }
-    ]
+    alias: [{ find: '@', replacement: '/src' }],
   },
-	server: {
-		host: '0.0.0.0'
-	},
-// TODO
+  server: {
+    host: '0.0.0.0',
+  },
+  // TODO
   // css: {
   //   preprocessorOptions: {
   //     sass: {
@@ -20,4 +18,4 @@ export default defineConfig({
   //     }
   //   }
   // }
-})
+});
