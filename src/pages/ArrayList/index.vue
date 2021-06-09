ad40a713a29856d1bfd356449cfcf31584156743
002002 <liaokaigogo@foxmail.com>
2021-06-09 19:35:23 +0800

    save

--- src/pages/ArrayList/index.vue
+++ src/pages/ArrayList/index.vue
@@ -14,7 +14,7 @@ import { ArrayList } from '@/components';
 import { onMounted, ref } from '@vue/runtime-core';
 import type { IArrayList } from '@/components/ArrayList/index.d';
 import { SortStatusEnum } from '@/components/ArrayList/index.d';
-const arrayList = ref<IArrayList>({} as IArrayList);
+const arrayList = ref({} as IArrayList);
 
 const initValue = [11, 13, 7, 12, 16, 9, 24, 5, 10, 3];
 // const initValue = [237, 146, 259, 348, 152, 163, 235, 48, 36, 62];
